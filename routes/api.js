1cf1c1f9c5cc526c8fbe36cac7aabd52c642d9de
Koding <teksu@teksu.koding.io>
2016-02-22 02:06:00 -0600

    api

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,22 @@ var proxyHost = 'http://staging.hailstone.io:3800';
 router.all('*', function(req, res, next) {
     var path = req.params[0];
     var proxyUrl = proxyHost + path;
+    if (req.query) {
+        var name = req.query.name;
+        var idx = req.query.idx;
+        var q = [];
+        if (name) {
+            q.push('name=' + name);
+        }
+        if (idx) {
+            q.push('idx=' + idx);
+        }
+        if (q.length > 0) {
+            proxyUrl += '?' + q.join('&');
+        }
+    }
+
+    console.log(proxyUrl);
     try {
         req.pipe(request(proxyUrl)).pipe(res);
     } catch (e) {
